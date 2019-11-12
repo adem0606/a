91a0e3d27ee5087520447173517e5c66c21791eb
adem0606 <57297046+adem0606@users.noreply.github.com>
2019-11-12 14:41:47 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,240 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "$";
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
+
+
+
+client.on("message", async message => {
+    var command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+        if(!message.channel.guild) return;
+            var args = message.content.split(" ").slice(1).join(" ");
+            if(command == "bc") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.forEach(member => {
+                                            let bc = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bc);
+                                        });
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
+                        });
+                            
+                        });
+            }
+            if(command == "bco") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
+                                            let bco = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bco);
+                                        });
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
+                        });
+                            
+                        });
+            }
+});
+
+
+
+client.on("message", async message => {
+    if(message.content == prefix + "server") {
+        if(!message.channel.guild) return;
+            if(!message.member.hasPermission("MANAGE_GUILD")) {
+                return message.channel.send("ليس لديك الصلآحية الكآفية . :broken_heart:");
+            }
+
+                let server = new Discord.RichEmbed()
+                    .setAuthor(message.guild.name)
+                    .setColor("RANDOM")
+                    .setTitle("Server Info :hearts: :sparkles:")
+                    .setDescription(`Members :bust_in_silhouette: : ${message.guild.memberCount}\nOwner :crown: : ${message.guild.owner.user.username}\nServer ID :id: : ${message.guild.id}\nRoles :lock: : ${message.guild.roles.size}\nRegion :earth_africa: : ${message.guild.region.toUpperCase()}`);
+
+                    message.channel.sendEmbed(server);
+
+    }
+});
+client.on("message", async message => {
+    if(message.content.startsWith(prefix + "banned")) {
+        if(!message.guild) return;
+        message.guild.fetchBans()
+        .then(bans => {
+            let b = bans.size;
+            let bb = bans.map(a => `${a}`).join(" - ");
+            message.channel.send(`**\`${b}\` | ${bb}**`);
+        });
+    }
+});
+client.on("message", async message => {
+    if(message.content.startsWith(prefix + "invite")) {
+        let invite = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setAuthor(message.author.username, message.author.displayAvatarURL)
+            .setThumbnail(message.author.avatarURL)
+            .setTitle("**Click Here To Invite The Bot To Your Server :sparkling_heart:**")
+            .setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
+            message.channel.sendEmbed(invite);
+    }
+});
+client.on("message", async message => {
+    if(message.content.startsWith(prefix + "help")) {
+        let help = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setThumbnail(message.author.avatarURL)
+            .setDescription(`**__برودكاست بوت __ 
+			
+            __برودكاست عادي__ : ${prefix}bc
+            __دعوة البوت لسيرفرك__ : ${prefix}invite
+            __معلومات عن السيرفر__ : ${prefix}server
+            __برودكاست للأونلاين فقط__ : ${prefix}bco
+			__لمسح الشات__ : ${prefix}clear
+            __يعرض لك عدد المتبندين من سيرفرك__ : ${prefix}banned
+            رابط سيرفر الدعم الفني: https://discord.gg/N85Cbma
+            **`);
+            message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
+    }
+});
+
+
+client.on("message", message => { //clear
+              var args = message.content.substring(prefix.length).split(" ");
+              if (message.content.startsWith(prefix + "clear")) {
+                  if(!message.channel.guild) return message.reply('**? اسف لكن هذا الامر للسيرفرات فقط **');         
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**?  لا يوجد لديك صلاحية لمسح الشات**');
+          var msg;
+          msg = parseInt();
+        
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+        message.channel.sendMessage("", {embed: {
+          title: "``تــم مسح الشات ``",
+          color: 0x5016f3, 
+          footer: {
+            
+          }
+        }}).then(msg => {msg.delete(3000)});
+                            }
+  
+       
+});
+
+const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+​
+const user = message.mentions.users.first(); // returns the user object if an user mention exists
+const banReason = args.slice(1).join(' '); // Reason of the ban (Everything behind the mention)
+​
+// Check if an user mention exists in this message
+if (!user) {
+try {
+// Check if a valid userID has been entered instead of a Discord user mention
+if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
+// If the client (bot) can get a user with this userID, it overwrites the current user variable to the user object that the client fetched
+user = message.guild.members.get(args.slice(0, 1).join(' '));
+user = user.user;
+} catch (error) {
+return message.reply('Couldn\' get a Discord user with this userID!');
+}
+}
+if (user === message.author) return message.channel.send('You can\'t ban yourself'); // Check if the user mention or the entered userID is the message author himsmelf
+if (!reason) return message.reply('You forgot to enter a reason for this ban!'); // Check if a reason has been given by the message author
+if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!'); // Check if the user is bannable with the bot's permissions
+​
+await message.guild.ban(user) // Bans the user
+​
+const Discord = require('discord.js'); // We need Discord for our next RichEmbeds
+const banConfirmationEmbed = new Discord.RichEmbed()
+.setColor('RED')
+.setDescription(`✅ ${user.tag} has been successfully banned!`);
+message.channel.send({
+embed: banConfirmationEmbed
+}); // Sends a confirmation embed that the user has been successfully banned
+​
+​
+const modlogChannelID = ''; // Discord channel ID where you want to have logged the details about the ban
+if (modlogChannelID.length !== 0) {
+if (!client.channels.get(modlogChannelID )) return undefined; // Check if the modlogChannelID is a real Discord server channel that really exists
+​
+const banConfirmationEmbedModlog = new Discord.RichEmbed()
+.setAuthor(`Banned by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
+.setThumbnail(user.displayAvatarURL)
+.setColor('RED')
+.setTimestamp()
+.setDescription(`**Action**: Ban
+**User**: ${user.username}#${user.discriminator} (${user.id})
+**Reason**: ${reason}`);
+client.channels.get(modlogChannelID).send({
+embed: banConfirmationEmbedModlog
+}); // Sends the RichEmbed in the modlogchannel
+}
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
